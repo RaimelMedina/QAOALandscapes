8209f42d1f4eb42d48607e3c8079311aa280d2a9
Raimel A. Medina <medinaramosar1992@gmail.com>
2023-04-03 17:17:07 +0200

    still in the docs v2

--- docs/make.jl
+++ docs/make.jl
@@ -2,47 +2,48 @@ using QAOALandscapes
 using Documenter
 using DocumenterMarkdown
 
-# DocMeta.setdocmeta!(QAOALandscapes, :DocTestSetup, :(using QAOALandscapes); recursive=true)
+DocMeta.setdocmeta!(QAOALandscapes, :DocTestSetup, :(using QAOALandscapes); recursive=true)
 
-# makedocs(;
-#     modules=[QAOALandscapes],
-#     authors="Raimel A. Medina",
-#     repo="https://github.com/RaimelMedina/QAOALandscapes.jl/blob/{commit}{path}#{line}",
-#     sitename="QAOALandscapes.jl",
-#     format=Documenter.HTML(;
-#         prettyurls=get(ENV, "CI", "false") == "true",
-#         canonical="https://RaimelMedina.github.io/QAOALandscapes.jl",
-#         edit_link="main",
-#         assets=String[],
-#     ),
-#     pages=[
-#         "QAOALandscapes" => "index.md",
-#         "api.md"
-#     ],
-# )
-
-# deploydocs(;
-#     repo="github.com/RaimelMedina/QAOALandscapes.jl",
-#     devbranch="main",
-# )
-
-makedocs(
-    sitename = "QAOALandscapes.jl",
-    #format = Documenter.HTML(
-    #    prettyurls = get(ENV, "CI", "false") == "true",
-    #    ansicolor = true
-    #),
-    format = Markdown(),
-    modules = [QAOALandscapes],
+makedocs(;
+    modules=[QAOALandscapes],
+    authors="Raimel A. Medina",
+    repo="https://github.com/RaimelMedina/QAOALandscapes.jl/blob/{commit}{path}#{line}",
+    sitename="QAOALandscapes.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://RaimelMedina.github.io/QAOALandscapes.jl",
+        edit_link="main",
+        assets=String[],
+    ),
     pages=[
         "QAOALandscapes" => "index.md",
         "api.md"
     ],
 )
 
-# # deploydocs(
-# #     repo   = "github.com/qu-co/QuantumNeuralNetworks.jl.git",
-# #     deps   = Deps.pip("mkdocs", "pygments", "python-markdown-math"),
-# #     make   = () -> run(`mkdocs build`)
-# #     target = "site"
-# # )
+deploydocs(;
+    repo="github.com/RaimelMedina/QAOALandscapes.jl",
+    devbranch="main",
+)
+
+# makedocs(
+#     sitename = "QAOALandscapes.jl",
+#     #format = Documenter.HTML(
+#     #    prettyurls = get(ENV, "CI", "false") == "true",
+#     #    ansicolor = true
+#     #),
+#     format = Markdown(),
+#     modules = [QAOALandscapes],
+#     pages=[
+#         "QAOALandscapes.jl" => "index.md",
+#         "api.md"
+#     ],
+# )
+
+# deploydocs(
+#     repo="github.com/RaimelMedina/QAOALandscapes.jl",    
+#     deps   = Deps.pip("mkdocs", "pygments", "python-markdown-math"),
+#     make   = () -> run(`mkdocs build`),
+#     devbranch="main",
+#     target = "site"
+# )
